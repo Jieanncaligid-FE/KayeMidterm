441fa4ad8ea5f2bd57e72841ecfa70bdd5b984ec
Jiean Caligid <jiean.caligid@example.com>
2026-04-10 20:12:00 +0800

    Refactor code structure for improved readability and maintainability

--- kaye/components/common/site-nav-links.js
+++ kaye/components/common/site-nav-links.js
@@ -2,5 +2,5 @@ export const siteNavLinks = [
   { href: "/", label: "Home" },
   { href: "/projects", label: "Projects" },
   { href: "/about", label: "About" },
-  { href: "/contact", label: "Contact" },
+  { href: "/contact", label: "Contact" }
 ];
